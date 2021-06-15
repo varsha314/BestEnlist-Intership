eeb53c40bc9584d0b30e9b930f63a22bb317e321
varsha314 <85819317+varsha314@users.noreply.github.com>
2021-06-15 11:55:04 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,32 @@
+# Assigning same value to 3 variables
+a = b = c = 50
+
+# Divide a by 10
+q = a / 10
+print(q)
+
+# Multiply b by 50
+p = b * 50
+print(b)
+
+# Add c value by 60
+s = c + 60
+print(s)
+
+# Replacing 3rd character of the string with G
+w = "Apple"
+w = w[:2] + 'G' + w[3:]
+print(w)
+
+# Two variables int and float type
+x, y = 10, 12.5
+
+print(x)
+print(y)
+
+# Converting int to float and float to int
+
+x = float(x)
+y = int(y)
+print(x)
+print(y)
