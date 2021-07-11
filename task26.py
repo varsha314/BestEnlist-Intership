410ce41649e2ae02924bd98331e62bf110a817a9
Varsha <85819317+varsha314@users.noreply.github.com>
2021-07-11 22:20:58 +0530

    Add files via upload

---
+++ task26.py
@@ -0,0 +1,9 @@
+# Enumerate a python list and print the counter with the list value
+list_1= ['Sridhar','Anu','Manasi','Adithya','Abilash']
+print("List:",list_1)
+print("Counter with list value:",list(enumerate(list_1)))
+
+# Enumerate a python tuple print the counter with the tuple value
+tuple_1=('Aravind','Bharath','Muthu','Sudhakar','Ayyanar')
+print("Tuple:",tuple_1)
+print("Counter with tuple value",tuple(enumerate(tuple_1)))
