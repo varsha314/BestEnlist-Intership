598e004082984b13e8ec0e3a8112f9089f5bf758
Varsha <85819317+varsha314@users.noreply.github.com>
2021-07-13 20:17:48 +0530

    Add files via upload

---
+++ task27.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import csv
+r = requests.get('https://authoraditiagarwal.com/')
+soup = BeautifulSoup(r.text, 'lxml')
+f = csv.writer(open(' dataprocessing.csv ','w'))
+f.writerow(['Title'])
+f.writerow([soup.title.text])
